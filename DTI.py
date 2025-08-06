909b7a5caa025a66eae49b7347b409dda1103ad5
Lital Cohen-Blum <litalcohen88@gmail.com>
2025-08-06 16:39:56 +0000

    reading QC dataset and exploring the data

--- DTI.py
+++ DTI.py
@@ -13,23 +13,70 @@
 #     name: python3
 # ---
 
+# %% [markdown]
+# # Accessing data
+
+# %% [markdown]
+# ### imports
+
 # %%
 from pathlib import Path
+from cloudpathlib import S3Path, S3Client
+import pandas as pd
 
-# Make the path object:
+
+
+# %% [markdown]
+# ### Loading preprocessed data
+
+# %%
+# Set up a local cache path
 cache_path = Path('/tmp/cache')
+cache_path.mkdir(exist_ok=True)
 
-# Just because we have made a cache path object doesn't mean that the directory
-# we made exists; here we check if it exists and make the directory if not.
-if not cache_path.exists():
-    cache_path.mkdir()
+# Set up the S3 client with anonymous access
+client = S3Client(local_cache_dir=cache_path, no_sign_request=True)
+
+# Define the base S3 path
+hbn_base_path_preproc = S3Path("s3://fcp-indi/", client=client)
+
+# Define the path to the curated dataset
+hbn_pod2_path = hbn_base_path_preproc / "data" / "Projects" / "HBN" / "BIDS_curated" / "derivatives"
+
+# List the first few items in the derivatives folder
+for item in hbn_pod2_path.iterdir():
+    print(item)
+
+# %% [markdown]
+# ### Exploring preprocessed data
 
 # %%
-import jupytext
-print(jupytext.__version__)
+participants_table = pd.read_csv(hbn_pod2_path / "qsiprep" / "participants.tsv", sep="\t")
+participants_table.head()
+tract_profiles = pd.read_csv(hbn_pod2_path / "afq" / "sub-NDARAA306NT2" / "ses-HBNsiteRU" / "sub-NDARAA306NT2_ses-HBNsiteRU_acq-64dir_space-T1w_desc-preproc_dwi_space-RASMM_model-CSD_desc-prob-afq_profiles.csv")
 
 # %%
-#test
-print('test2')
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+ax.plot(tract_profiles[tract_profiles["tractID"] == "CST_L"]["dki_fa"].values)
+ax.set_xlabel("Node")
+ax.set_ylabel("FA")
+
+# %%
+participants_table.head()
+
+# %%
+participants_table.describe()
+
+# %%
+participants_table.hist(figsize=(30,30), bins=50)
+
+# %%
+tract_profiles.head()
+
+# %%
+tract_profiles.describe()
+
+# %%
 
 # %%
